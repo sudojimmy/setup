96ee77099d73fbd795132320ee4b42c3a1b92bb6
Jiaming Zhang <jason.jiaming.zhang@huawei.com>
2019-01-21 09:37:42 -0500

    add tmux config file

--- my-rc-files/setup.sh
+++ my-rc-files/setup.sh
@@ -1,11 +1,11 @@
-sudo apt-get install git
-sudo apt-get install vim-common 
-sudo apt-get install vim-doc 
-sudo apt-get install vim-runtime 
-sudo apt-get install vim-gnome 
-git clone https://ZhuoxiLi@github.com/YLiLarry/cs246-heartstone.git ~
-sudo cp .bashrc ~
-sudo cp .vimrc ~
+# use sudo to run script
+apt-get install git
+apt-get install vim-common
+apt-get install vim-doc
+apt-get install vim-runtime
+apt-get install vim-gnome
+cp .bashrc ~
+cp .vimrc ~
+cp .tmux.conf ~
 mkdir ~/.go-dirs
-cd ~/cs246-heartstone
 source ~/.bashrc
